a1bc89e48fbaa041cc497d9b0296f41a24f32e31
yahyaBir <yahyabirr61@gmail.com>
2023-08-01 16:30:38 +0300

    deneme son commit

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -20,7 +20,7 @@ class LoginController extends Controller
         $username = $request->input('username');
         $password = $request->input('password');
 
-        $user = User::where('username', $username)->first();
+         $user= User::where('username', $username)->first();
 
         if (Auth::attempt(['username'=>$username, 'password'=>$password])) {
             return redirect()->route('main-view');
